6c44411429fb68fc963839f201c5a988c5c51292
Sam <samstarkscenic@gmail.com>
2020-11-20 22:56:51 -0500

    adds login route

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username:{
@@ -76,5 +79,52 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function(models) {
     // associations can be defined here
   };
+
+  //
+  // ─── USER MODEL METHODS ─────────────────────────────────────────────────────────
+  //
+
+  User.prototype.toSafeObject = function() {
+    const { id, username, email, firstName, lastName, profilePicURL } = this;
+    return { id, username, email, firstName, lastName, profilePicURL };
+  };
+
+  User.prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
+   };
+
+  User.getCurrentUserById = async function (id) {
+  return await User.scope('currentUser').findByPk(id);
+  };
+
+  User.login = async function ({ credential, password }) {
+    const { Op } = require('sequelize');
+    const user = await User.scope('loginUser').findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential,
+        },
+      },
+    });
+    if (user && user.validatePassword(password)) {
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+  };
+
+  User.signup = async function ({ username, email, firstName, lastName, profilePicURL, password }) {
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+      username,
+      email,
+      firstName,
+      lastName,
+      profilePicURL,
+      hashedPassword,
+    });
+    return await User.scope('currentUser').findByPk(user.id);
+  };
+
+
   return User;
 };
