09f5103bd7f6fcac12446a6f48c0c9e3f113c73f
Sam <samstarkscenic@gmail.com>
2020-11-20 23:13:05 -0500

    adds validateLogin() as middleware for login route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+//
+// ─── MIDDLEWARE FUNCTIONS ───────────────────────────────────────────────────────
+//
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+];
 
 //
 // ─── LOGIN ──────────────────────────────────────────────────────────────────────
@@ -13,6 +30,7 @@ const router = express.Router();
 
 router.post(
   '/',
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
