1aa41f2813428ec2d8b917bd863a6a4d20357c37
Sam Stark <SamStarkScenic@gmail.com>
2020-11-23 13:25:45 -0500

    adds simple routes for testing event post and get

---
+++ backend/routes/api/events.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { Event, User } = require('../../db/models');
+
+const {
+  s3,
+  singlePublicFileUpload,
+  singleMulterUpload,
+} = require("../../utils/awsS3");
+
+const router = express.Router();
+
+//
+// ─── MIDDLEWARE FUNCTIONS ───────────────────────────────────────────────────────
+//
+
+const validateEvent = [
+  check('title')
+    .exists({ checkFalsy: true })
+    .isLength({min: 4, max: 50})
+    .withMessage('Please provide an event title between 4 and 50 characters in length.'),
+  check('summary')
+    .exists({ checkFalsy: true })
+    .isLength({min: 5, max: 120})
+    .withMessage('Please provide an event summary between 5 and 50 characters in length.'),
+  // Checks whether or not the organizer user's id exists in database
+  check('organizer')
+    .exists({checkFalsy: true })
+    .custom(value => {
+      return User.findByPk(value).then(user => {
+        if (!user) return Promise.reject('You must be logged in as a valid user to create an event!')
+        return true
+      })
+    }),
+  handleValidationErrors,
+];
+
+router.post(
+  '',
+  singleMulterUpload('eventPic'),
+  requireAuth,
+  validateEvent,
+  asyncHandler(async (req, res) => {
+    const {title, summary, about, organizer} = req.body;
+
+    let eventPicURL = null;
+    if (req.file) eventPicURL = await singlePublicFileUpload(req.file, 'event-pics')
+
+    const event = await Event.create({title, summary, about, eventPicURL, organizer})
+
+    return res.json({event})
+  })
+)
+
+router.get(
+  '',
+  asyncHandler(async (req, res) => {
+    const events = await Event.findAll()
+
+    return res.json( {events} )
+  })
+)
+
+module.exports = router
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const eventsRouter = require('./events.js');
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+router.use('/events', eventsRouter);
 
 module.exports = router;
