001f804996a46f855d4f263174db51edac2a9b7f
Sam Stark <SamStarkScenic@gmail.com>
2020-11-23 19:32:24 -0500

    adds tickets model

---
+++ backend/db/migrations/20201123224355-create-ticket.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Tickets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      eventId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Events',
+          key: 'id'
+        }
+      },
+      name: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      sold: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Tickets');
+  }
+};
---
+++ backend/db/models/ticket.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Ticket = sequelize.define('Ticket', {
+    eventId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    string: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        max: this.quantity
+      }
+    },
+  }, {});
+  Ticket.associate = function (models) {
+    // associations can be defined here
+  };
+  return Ticket;
+};
