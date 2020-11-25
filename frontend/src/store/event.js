0a5b325023c7bf7c63713ee086fff7eab999bfe2
Sam Stark <SamStarkScenic@gmail.com>
2020-11-25 13:14:53 -0500

    starts events redux store

--- frontend/src/store/event.js
+++ frontend/src/store/event.js
@@ -1,6 +1,55 @@
 import Axios from 'axios'
 
-const ADD_EVENT = 'events/addEvent'
+import { fetch } from './csrf'
+
+const ADD_EVENTS = 'events/addEvents'
+const SET_EVENTS = 'events/setEvents'
+const GET_X_EVENTS = 'events/getXEvents'
+const GET_ALL_EVENTS = 'events/getALLEvents'
+const CLEAR_EVENTS = 'events/clearEvents'
+
+//events is an array
+const setEvents = (events) => ({
+  type: SET_EVENTS,
+  payload: events
+})
+
+//events is an array
+const addEvents = (events) => ({
+  type: ADD_EVENTS,
+  payload: events
+})
+
+const clearEvents = () => ({
+  type: CLEAR_EVENTS
+})
+
+const getXEvents = (start = 0, amount = 10) => ({
+  type: GET_X_EVENTS,
+  payload: {
+    start,
+    amount
+  }
+})
+
+const getAllEvents = () => ({
+  type: GET_ALL_EVENTS,
+})
+
+export const getCurrentEvents = (start, amount) => {
+  return dispatch => {
+    return dispatch(getXEvents(start, amount))
+  }
+}
+
+export const loadMoreEvents = (start, amount) => {
+  return async dispatch => {
+    const res = await fetch(`/api/events?start=${start}&amount=${amount}`)
+
+    return dispatch(addEvents(res.data))
+    return dispatch(getAllEvents())
+  }
+}
 
 export const createNewEvent = (event) => {
   const { title, summary, organizer, about, eventPic, tickets } = event
@@ -16,10 +65,6 @@ export const createNewEvent = (event) => {
   if (eventPic) {
     formData.append('eventPic', eventPic)
   }
-
-  // tickets.forEach((ticket, idx) => {
-  //   formData.append(`ticket${idx}`, JSON.stringify(ticket))
-  // })
 
   const config = {
     headers: {
@@ -40,9 +85,20 @@ const initialState = {
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
+    case GET_X_EVENTS:
+      return state.events.slice(payload.start, payload.start + payload.amount)
 
-    case ADD_EVENT:
+    case GET_ALL_EVENTS:
+      return state.events
+
+    case SET_EVENTS:
+      return { ...payload }
+
+    case ADD_EVENTS:
       return { ...state, ...payload }
+
+    case CLEAR_EVENTS:
+      return { events: null }
 
     default:
       return state
